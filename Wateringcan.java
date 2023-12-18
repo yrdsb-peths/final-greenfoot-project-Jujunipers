c672313e4a221863b3ac5da7367cc49767f27c2d
Jujunipers <150176649+Jujunipers@users.noreply.github.com>
2023-12-18 12:40:37 -0500

    Fixed wateringCan bugs
    1. NullPointerException error when watering can is dragged onto empty pot - fixed if statement in Wateringcan class
    2. Watering can would get stuck under pot once planted - fixed if statement in Wateringcan class

--- Wateringcan.java
+++ Wateringcan.java
@@ -27,11 +27,9 @@ public class Wateringcan extends Actor
                 
                 Pot potInstance = (Pot) getOneIntersectingObject(Pot.class); // gets the specific pot instance that the mouse is touching
                 // If wateringCan is over plant by end of drag, water plant, but only if thirsty. If not thirsty, return wateringCan to og location
-                if(isTouching(Pot.class)) {
-                    if(potInstance.plant.thirsty) {
-                        potInstance.plant.waterPlant();
-                        setLocation(100, 100); // temporary, replace with watering animation
-                    }
+                if(isTouching(Pot.class) && isTouching(Plant.class) && potInstance.plant.thirsty) {
+                    potInstance.plant.waterPlant();
+                    setLocation(100, 100); // temporary, replace with watering animation
                 } else {
                     setLocation(ogX, ogY); // return wateringCan to og location when drag is released
                 }
